d75a494f385d0bf18c04622c5e24a7e298c66a6f
Rick-26R <ramon.rodriguez2622@mor.conalep.edu.mx>
2024-05-22 20:39:51 -0600

    Add: Feat to look the password and hide it in login and signup page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,91 +1,97 @@
-import Button from '@mui/material/Button';
-import DeleteIcon from '@mui/icons-material/Delete';
-import SendIcon from '@mui/icons-material/Send';
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
-import { Box, Container, TextField } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react';
+import { Button, Box, Container, TextField, IconButton, Typography, InputAdornment } from '@mui/material';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import Link from 'next/link';
 
+export default function Index() {
+  const [showPassword, setShowPassword] = useState(false);
 
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
-
-export default function index() {
-    return (
-        <>
-            <Container
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height: '100vh',
-                    flexDirection: 'column',
-
-                }}
+  return (
+    <>
+      <Container
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+          flexDirection: 'column',
+        }}
+      >
+        <Box
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            flexDirection: 'column',
+          }}
+        >
+          <form>
+            <Box
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '50vh',
+                padding: '20px',
+                flexDirection: 'column',
+                backgroundColor: 'rgba(255, 255, 255, 0.1)',
+                borderRadius: '10px',
+              }}
             >
-                <Box
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        height: '100vh',
-                        flexDirection: 'column',
-                    }}
-                >
-                    <form>
-                        <Box
-                            style={{
-                                display: 'flex',
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                height: '50vh',
-                                padding: '20px',
-                                flexDirection: 'column',
-                                backgroundColor: 'rgba(255, 255, 255, 0.1)',
-                                borderRadius: '10px',
-                            }}
-                        >
-                            <Typography variant="h1" fontSize={30}>
-                                Iniciar Sesion
-                            </Typography>
+              <Typography variant="h1" fontSize={30}>
+                Iniciar Sesión
+              </Typography>
 
-                            <TextField id="correo-basic" label="Correo" variant="outlined"
-                                style={{
-                                    margin: '20px',
-                                    width: '90%',
-                                }}
-                            />
-                            <TextField id="Password-basic" label="Password" variant="outlined"
-                            type="password"
-                                style={{
-                                    marginBottom: '30px',
-                                    width: '90%',
-                                }}
-                            />
+              <TextField
+                id="correo-basic"
+                label="Correo"
+                variant="outlined"
+                style={{
+                  margin: '20px',
+                  width: '90%',
+                }}
+              />
+              <TextField
+                id="Password-basic"
+                label="Password"
+                variant="outlined"
+                type={showPassword ? 'text' : 'password'}
+                style={{
+                  marginBottom: '30px',
+                  width: '90%',
+                }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
 
-                            <Button color="success" size="large" variant="outlined"
-                                width="100%"
-                            >
-                                Iniciar Sesion
-                            </Button>
+              <Button color="success" size="large" variant="outlined" fullWidth>
+                Iniciar Sesión
+              </Button>
 
-                            <Typography variant="p" fontSize={18}
-                                style={{
-                                    marginTop: '10px',
-                                }}
-                            >
-                                ¿No tienes cuenta? <Link href="/signup"
-                                    style={{
-                                        textDecoration: 'none',
-                                        color: '#388e3c',
-                                    }}
-                                >Registrate</Link>
-                            </Typography>
-                        </Box>
-
-                    </form>
-                </Box >
-            </Container >
-        </>
-    )
+              <Typography variant="body1" fontSize={18} style={{ marginTop: '10px' }}>
+                ¿No tienes cuenta? <Link href="/signup" style={{ textDecoration: 'none', color: '#388e3c' }}>Regístrate</Link>
+              </Typography>
+            </Box>
+          </form>
+        </Box>
+      </Container>
+    </>
+  );
 }
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,117 +1,160 @@
+import React from 'react';
+import { useState } from 'react';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { Box, Container, TextField } from '@mui/material'
-import React from 'react'
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import { Box, Container, TextField, IconButton, InputAdornment } from '@mui/material';
 import Link from 'next/link';
 
+export default function Index() {
+  const [values, setValues] = useState({
+    password: '',
+    confirmPassword: '',
+    showPassword: false,
+    showConfirmPassword: false,
+  });
 
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
 
+  const handleClickShowConfirmPassword = () => {
+    setValues({ ...values, showConfirmPassword: !values.showConfirmPassword });
+  };
 
-export default function index() {
-    return (
-        <>
-            <Container
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height: '100vh',
-
-
-                }}
+  return (
+    <>
+      <Container
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            flexDirection: 'column',
+          }}
+        >
+          <form>
+            <Box
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '65vh',
+                padding: '20px',
+                flexDirection: 'column',
+                backgroundColor: 'rgba(255, 255, 255, 0.1)',
+                borderRadius: '10px',
+              }}
             >
-                <Box
-                    sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        height: '100vh',
-                        flexDirection: 'column',
-
-                    }}
+              <Container>
+                <Typography variant="h1" fontSize={30}
+                  style={{
+                    marginBottom: '20px',
+                    textAlign: 'center',
+                  }}
                 >
-                    <form>
-                        <Box
-                            style={{
-                                display: 'flex',
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                height: '65vh',
-                                padding: '20px',
-                                flexDirection: 'column',
-                                backgroundColor: 'rgba(255, 255, 255, 0.1)',
-                                borderRadius: '10px',
-                            }}
-                        >
-                            <Container>
-                                <Typography variant="h1" fontSize={30}
-                                    style={{
-                                        marginBottom: '20px',
-                                        textAlign: 'center',
-                                    }}
-                                >
-                                    Reagistrate
-                                </Typography>
-                                <TextField
+                  Reagistrate
+                </Typography>
+                <TextField
+                  id="demo-helper-text-aligned"
+                  label="Nombre"
+                  style={{
+                    marginRight: '5px',
+                    marginBottom: '10px',
+                  }}
+                />
+                <TextField
+                  id="demo-helper-text-aligned-no-helper"
+                  label="Apellidos"
+                />
+              </Container>
+              <TextField
+                id="correo-basic"
+                label="Correo"
+                variant="outlined"
+                style={{
+                  margin: '10px',
+                  width: '90%',
+                }}
+              />
+              <TextField
+                id="Contraseña-basic"
+                label="Contraseña"
+                variant="outlined"
+                type={values.showPassword ? 'text' : 'password'}
+                style={{
+                  margin: '10px',
+                  width: '90%',
+                }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        edge="end"
+                      >
+                        {values.showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
 
-                                    id="demo-helper-text-aligned"
-                                    label="Nombre"
-                                    style={{
-                                        marginRight: '5px',
-                                        marginBottom: '10px',
-                                    }}
-                                />
-                                <TextField
+              <TextField
+                id="Confirmar-basic"
+                label="Confirmar contraseña"
+                variant="outlined"
+                type={values.showConfirmPassword ? 'text' : 'password'}
+                style={{
+                  margin: '10px',
+                  width: '90%',
+                }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle confirm password visibility"
+                        onClick={handleClickShowConfirmPassword}
+                        edge="end"
+                      >
+                        {values.showConfirmPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
 
-                                    id="demo-helper-text-aligned-no-helper"
-                                    label="Apellidos"
-                                />
-                            </Container>
-                            <TextField id="correo-basic" label="Correo" variant="outlined"
-                                style={{
-                                    margin: '10px',
-                                    width: '90%',
-                                }}
-                            />
-                            <TextField id="Contraseña-basic" label="Contraseña" variant="outlined"
-                            type="password"
-                                style={{
-                                    margin: '10px',
-                                    width: '90%',
-                                }}
-                            />
-                            <TextField id="Confirmar-basic" label="Confirmar contraseña"
-                            type="password"
-                            variant="outlined"
-                                style={{
-                                    margin: '10px',
-                                    width: '90%',
-                                }}
-                            />
-                            <Button color="success" size="large" variant="outlined"  
-                                width="100%"
-                            >
-                                registrarse
-                            </Button>
-                            <Typography variant="p" fontSize={18}
-                                style={{
-                                    marginTop: '10px',
-                                }}
-                            >
-                                ¿Ya eres usuario? <Link href="/"
-                                    style={{
-                                        textDecoration: 'none',
-                                        color: '#388e3c',
-                                    }}
-                                >Iniciar sesión</Link>
-                            </Typography>
-                        </Box>
-
-
-                    </form>
-
-                </Box>
-            </Container >
-        </>
-    )
+              <Button color="success" size="large" variant="outlined"
+                width="100%"
+              >
+                registrarse
+              </Button>
+              <Typography variant="p" fontSize={18}
+                style={{
+                  marginTop: '10px',
+                }}
+              >
+                ¿Ya eres usuario? <Link href="/"
+                  style={{
+                    textDecoration: 'none',
+                    color: '#388e3c',
+                  }}
+                >Iniciar sesión</Link>
+              </Typography>
+            </Box>
+          </form>
+        </Box>
+      </Container>
+    </>
+  );
 }
