23a69deadd00ba5d6fcb9d63aa708c57c26f4274
FReptar0 <20213tn137@utez.edu.mx>
2024-06-01 11:41:04 -0600

    feat: Update Deposits component with expected income section

--- src/components/Deposits.js
+++ src/components/Deposits.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import Title from './Title';
+import { Box } from '@mui/material';
 
 function preventDefault(event) {
     event.preventDefault();
@@ -10,18 +11,15 @@ function preventDefault(event) {
 export default function Deposits() {
     return (
         <React.Fragment>
-            <Title>Recent Deposits</Title>
-            <Typography component="p" variant="h4">
-                $3,024.00
-            </Typography>
-            <Typography color="text.secondary" sx={{ flex: 1 }}>
-                on 15 March, 2019
-            </Typography>
-            <div>
-                <Link color="primary" href="#" onClick={preventDefault}>
-                    View balance
-                </Link>
-            </div>
+            <Box sx={{ alignItems: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                <Title>Ingresos expectados</Title>
+                <Typography component="p" variant="h4">
+                    $3,024.00
+                </Typography>
+                <Typography color="text.secondary" sx={{ flex: 1 }}>
+                    on 15 March, 2019
+                </Typography>
+            </Box>
         </React.Fragment>
     );
 }
