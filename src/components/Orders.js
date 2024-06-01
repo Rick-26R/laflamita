81ec9000041e7f099ed10c75a810f9efde8bdda0
FReptar0 <20213tn137@utez.edu.mx>
2024-06-01 11:41:46 -0600

    feat: Update orders component with Title and delete payment method

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -8,8 +8,8 @@ import TableRow from '@mui/material/TableRow';
 import Title from './Title';
 
 // Generate Order Data
-function createData(id, date, name, shipTo, paymentMethod, amount) {
-    return { id, date, name, shipTo, paymentMethod, amount };
+function createData(id, date, name, shipTo, amount) {
+    return { id, date, name, shipTo, amount };
 }
 
 const rows = [
@@ -18,7 +18,6 @@ const rows = [
         '16 Mar, 2019',
         'Elvis Presley',
         'Tupelo, MS',
-        'VISA ⠀•••• 3719',
         312.44,
     ),
     createData(
@@ -26,16 +25,14 @@ const rows = [
         '16 Mar, 2019',
         'Paul McCartney',
         'London, UK',
-        'VISA ⠀•••• 2574',
         866.99,
     ),
-    createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
+    createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 100.81),
     createData(
         3,
         '16 Mar, 2019',
         'Michael Jackson',
         'Gary, IN',
-        'AMEX ⠀•••• 2000',
         654.39,
     ),
     createData(
@@ -43,26 +40,21 @@ const rows = [
         '15 Mar, 2019',
         'Bruce Springsteen',
         'Long Branch, NJ',
-        'VISA ⠀•••• 5919',
         212.79,
     ),
 ];
 
-function preventDefault(event) {
-    event.preventDefault();
-}
 
 export default function Orders() {
     return (
         <React.Fragment>
-            <Title>Recent Orders</Title>
+            <Title>Ordenes Recientes</Title>
             <Table size="small">
                 <TableHead>
                     <TableRow>
                         <TableCell>Date</TableCell>
                         <TableCell>Name</TableCell>
                         <TableCell>Ship To</TableCell>
-                        <TableCell>Payment Method</TableCell>
                         <TableCell align="right">Sale Amount</TableCell>
                     </TableRow>
                 </TableHead>
@@ -72,14 +64,13 @@ export default function Orders() {
                             <TableCell>{row.date}</TableCell>
                             <TableCell>{row.name}</TableCell>
                             <TableCell>{row.shipTo}</TableCell>
-                            <TableCell>{row.paymentMethod}</TableCell>
                             <TableCell align="right">{`$${row.amount}`}</TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
             </Table>
-            <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
-                See more orders
+            <Link color="primary" href="/orders" sx={{ mt: 3 }}>
+                Ver mas ordenes
             </Link>
         </React.Fragment>
     );
