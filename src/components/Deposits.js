8febb1ca25d0119846677ac633e7559842b3284f
FReptar0 <20213tn137@utez.edu.mx>
2024-06-01 14:24:23 -0600

    fix: title in Deposits component

--- src/components/Deposits.js
+++ src/components/Deposits.js
@@ -4,15 +4,11 @@ import Typography from '@mui/material/Typography';
 import Title from './Title';
 import { Box } from '@mui/material';
 
-function preventDefault(event) {
-    event.preventDefault();
-}
-
 export default function Deposits() {
     return (
         <React.Fragment>
             <Box sx={{ alignItems: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-                <Title>Ingresos expectados</Title>
+                <Title>Ingresos esperados</Title>
                 <Typography component="p" variant="h4">
                     $3,024.00
                 </Typography>
