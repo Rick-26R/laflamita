ece5131e7d2487cfa9fd1826fd233b4497e4e04c
FReptar0 <20213tn137@utez.edu.mx>
2024-06-01 21:14:03 -0600

    feat: Add inventory link to main navigation

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -16,38 +16,38 @@ import { Search } from '@mui/icons-material';
 import { ButtonsNotPaid } from './ButtonsOrders';
 import { ButtonsPaid } from './ButtonsOrders';
 
-function createData(id, date, name, shipTo, amount, status) {
-    return { id, date, name, shipTo, amount, status };
+function createData(id, date, name, amount, status) {
+    return { id, date, name, amount, status };
 }
 
 
 const rows = [
-    createData(0, '16 Mar, 2019', 'Elvis Presley', 'Tupelo, MS', 312.44, 'Esperando pago'),
-    createData(1, '16 Mar, 2019', 'Paul McCartney', 'London, UK', 866.99, 'Esperando pago'),
-    createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 100.81, 'Esperando pago'),
-    createData(3, '16 Mar, 2019', 'Michael Jackson', 'Gary, IN', 654.39, 'Esperando pago'),
-    createData(4, '15 Mar, 2019', 'Bruce Springsteen', 'Long Branch, NJ', 212.79, 'Esperando pago'),
-    createData(5, '15 Mar, 2019', 'Whitney Houston', 'Newark, NJ', 150.00, 'Esperando pago'),
-    createData(6, '14 Mar, 2019', 'Janis Joplin', 'Port Arthur, TX', 400.67, 'Esperando pago'),
-    createData(7, '13 Mar, 2019', 'Jimi Hendrix', 'Seattle, WA', 820.42, 'Esperando pago'),
-    createData(8, '13 Mar, 2019', 'Kurt Cobain', 'Aberdeen, WA', 732.18, 'Esperando pago'),
-    createData(9, '12 Mar, 2019', 'Jim Morrison', 'Melbourne, FL', 319.29, 'Esperando pago'),
-    createData(10, '12 Mar, 2019', 'John Lennon', 'Liverpool, UK', 912.34, 'Esperando pago'),
-    createData(11, '11 Mar, 2019', 'Freddie Mercury', 'Stone Town, Tanzania', 615.67, 'Esperando pago'),
-    createData(12, '11 Mar, 2019', 'David Bowie', 'London, UK', 732.81, 'Pagado'),
-    createData(13, '10 Mar, 2019', 'Prince', 'Minneapolis, MN', 501.45, 'Pagado'),
-    createData(14, '10 Mar, 2019', 'Tina Turner', 'Nutbush, TN', 673.12, 'Pagado'),
-    createData(15, '09 Mar, 2019', 'Axl Rose', 'Lafayette, IN', 342.79, 'Pagado'),
-    createData(16, '09 Mar, 2019', 'Billy Joel', 'New York, NY', 423.67, 'Pagado'),
-    createData(17, '08 Mar, 2019', 'Madonna', 'Bay City, MI', 678.99, 'Pagado'),
-    createData(18, '08 Mar, 2019', 'Stevie Wonder', 'Saginaw, MI', 812.44, 'Pagado'),
-    createData(19, '07 Mar, 2019', 'Elton John', 'Pinner, UK', 765.67, 'Pagado'),
-    createData(20, '07 Mar, 2019', 'Bob Dylan', 'Duluth, MN', 541.39, 'Pagado'),
-    createData(21, '06 Mar, 2019', 'Ray Charles', 'Albany, GA', 332.44, 'Pagado'),
-    createData(22, '06 Mar, 2019', 'James Brown', 'Barnwell, SC', 292.68, 'Pagado'),
-    createData(23, '05 Mar, 2019', 'Aretha Franklin', 'Memphis, TN', 423.78, 'Pagado'),
-    createData(24, '05 Mar, 2019', 'Mick Jagger', 'Dartford, UK', 754.12, 'Pagado'),
-    createData(25, '04 Mar, 2019', 'Eric Clapton', 'Ripley, UK', 612.30, 'Pagado'),
+    createData(0, '16 Mar, 2019', 'Elvis Presley', 312.44, 'Esperando pago'),
+    createData(1, '16 Mar, 2019', 'Paul McCartney', 866.99, 'Esperando pago'),
+    createData(2, '16 Mar, 2019', 'Tom Scholz', 100.81, 'Esperando pago'),
+    createData(3, '16 Mar, 2019', 'Michael Jackson', 654.39, 'Esperando pago'),
+    createData(4, '15 Mar, 2019', 'Bruce Springsteen', 212.79, 'Esperando pago'),
+    createData(5, '15 Mar, 2019', 'Whitney Houston', 150.00, 'Esperando pago'),
+    createData(6, '14 Mar, 2019', 'Janis Joplin', 400.67, 'Esperando pago'),
+    createData(7, '13 Mar, 2019', 'Jimi Hendrix', 820.42, 'Esperando pago'),
+    createData(8, '13 Mar, 2019', 'Kurt Cobain', 732.18, 'Esperando pago'),
+    createData(9, '12 Mar, 2019', 'Jim Morrison', 319.29, 'Esperando pago'),
+    createData(10, '12 Mar, 2019', 'John Lennon', 912.34, 'Esperando pago'),
+    createData(11, '11 Mar, 2019', 'Freddie Mercury', 615.67, 'Esperando pago'),
+    createData(12, '11 Mar, 2019', 'David Bowie', 732.81, 'Pagado'),
+    createData(13, '10 Mar, 2019', 'Prince Lastname', 501.45, 'Pagado'),
+    createData(14, '10 Mar, 2019', 'Tina Turner', 673.12, 'Pagado'),
+    createData(15, '09 Mar, 2019', 'Axl Rose', 342.79, 'Pagado'),
+    createData(16, '09 Mar, 2019', 'Billy Joel', 423.67, 'Pagado'),
+    createData(17, '08 Mar, 2019', 'Madonna Lastname', 678.99, 'Pagado'),
+    createData(18, '08 Mar, 2019', 'Stevie Wonder', 812.44, 'Pagado'),
+    createData(19, '07 Mar, 2019', 'Elton John', 765.67, 'Pagado'),
+    createData(20, '07 Mar, 2019', 'Bob Dylan', 541.39, 'Pagado'),
+    createData(21, '06 Mar, 2019', 'Ray Charles', 332.44, 'Pagado'),
+    createData(22, '06 Mar, 2019', 'James Brown', 292.68, 'Pagado'),
+    createData(23, '05 Mar, 2019', 'Aretha Franklin', 423.78, 'Pagado'),
+    createData(24, '05 Mar, 2019', 'Mick Jagger', 754.12, 'Pagado'),
+    createData(25, '04 Mar, 2019', 'Eric Clapton', 612.30, 'Pagado'),
 ];
 
 const rowsPerPageOptions = rows.map((row, index) => {
@@ -87,7 +87,6 @@ function stableSort(array, comparator) {
 const headCells = [
     { id: 'date', numeric: false, disablePadding: false, label: 'Fecha' },
     { id: 'name', numeric: false, disablePadding: false, label: 'Nombre' },
-    { id: 'shipTo', numeric: false, disablePadding: false, label: 'Tipo' },
     { id: 'amount', numeric: true, disablePadding: false, label: 'Total' },
     { id: 'status', numeric: false, disablePadding: false, label: 'Estado' },
 ];
@@ -148,7 +147,6 @@ export default function Orders(props) {
                 rows.filter((row) =>
                     row.name.toLowerCase().includes(searchText.toLowerCase()) ||
                     row.date.toLowerCase().includes(searchText.toLowerCase()) ||
-                    row.shipTo.toLowerCase().includes(searchText.toLowerCase()) ||
                     row.amount.toString().toLowerCase().includes(searchText.toLowerCase()) ||
                     row.status.toLowerCase().includes(searchText.toLowerCase())
                 )
@@ -223,7 +221,6 @@ export default function Orders(props) {
                                 <TableRow key={row.id}>
                                     <TableCell>{row.date}</TableCell>
                                     <TableCell>{row.name}</TableCell>
-                                    <TableCell>{row.shipTo}</TableCell>
                                     <TableCell align="right">{`$${row.amount}`}</TableCell>
                                     <TableCell>{row.status}</TableCell>
                                     <TableCell
