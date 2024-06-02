9ac4e1b06307e8fe8f7c6cba1f26eb3623789d05
FReptar0 <20213tn137@utez.edu.mx>
2024-06-01 20:38:06 -0600

    chore: Update providers page layout and add Providers component

---
+++ src/components/ButtonsProvider.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import { red, green, blue, grey, amber } from '@mui/material/colors';
+import { Delete, Check, Visibility, LocalPrintshop, Edit } from '@mui/icons-material';
+
+export function Actions(props) {
+    return (
+        <>
+            <IconButton
+                aria-label="view"
+                onClick={() => {
+                    console.log('Ver');
+                }}
+            >
+                <Tooltip title="Ver proveedor">
+                    <Visibility
+                        sx={{
+                            color: blue[900]
+                        }}
+                    />
+                </Tooltip>
+            </IconButton>
+
+            <IconButton
+                aria-label="print"
+                onClick={() => {
+                    console.log('Imprimir');
+                }}
+            >
+                <Tooltip title="Editar tarjeta">
+                    <Edit
+                        sx={{
+                            color: amber[900]
+                        }}
+                    />
+                </Tooltip>
+            </IconButton>
+        </>
+    )
+}
